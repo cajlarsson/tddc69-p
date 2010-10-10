2dfb2f83ab598a9a0eedd6ca0e159e092ee52153
Caj Larsson <caj.larsson@gmail.com>
2010-10-10 16:40:04 +0200

    added missing file

---
+++ caveexplorer/client/MoveUnitMessage.java
@@ -0,0 +1,25 @@
+package caveexplorer.client;
+
+import java.io.*;
+public class MoveUnitMessage extends UnitMessage
+{
+	private Units unitType;
+	private Position position;
+	public MoveUnitMessage(MessageType type, int ID,
+				 Position position, Units unitType)
+	{
+		super(type, ID);
+		this.position = position;
+		this.unitType = unitType;
+	}
+	
+	public Position getPosition()
+	{
+		return position;
+	}
+	
+	public Units getUnits()
+	{
+		return unitType
+	}
+}
